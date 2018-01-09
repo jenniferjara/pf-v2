436f7578ea825327c382b767b481cae50b675cc5
Jennifer Jara <Jennifer Jara>
2018-01-08 21:35:11 -0500

    add animation demos_more section

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,35 @@
 (function($) {
 
     'use strict';
-    $(window).resize(function () {
-        console.log($(window).width());
-    });
+
+    /*var scrollMenus = function (e) {
+        e.preventDefault();
+        this.each(function () {
+            $('html, body').animate({
+                scrollTop: $(this).offset().top
+            }, 1000);
+        });
+    };*/
 
     var showDemos = function (e) {
         e.preventDefault();
-        console.log($(this).text());
-
-        $('.demo_bg:nth-child(2)').toggle('slow');
-        if ($(this).text() == "Ver más") {
-            $(this).text("Ver menos");
+        //$('.demo_bg:nth-child(2)').toggle('slow');
+        if ($(this).text() == "+") {
+            $(this).text("-");
+            $(this).parent().addClass('demo_more_tp');
+            $('.demo_bg:nth-child(2)').css('display', 'block');
+            $("html, body").animate({ scrollTop: ($('.demo_bg:nth-child(1)').offset().top + $('.demo_bg:nth-child(1)').height()) - 70 }, 1000);
+            resizes();
         } else {
-            $(this).text('Ver más');
+            $(this).text('+');
+            $(this).parent().removeClass('demo_more_tp');
+            $("html, body").animate({ scrollTop: ($('.demo_bg:nth-child(1)').offset().top) - 70}, 1400);
+            $('.demo_bg:nth-child(2)').css('display', 'none');
         }
+
     };
 
-	var scrollMenu = function() {
+	var scrollNav = function() {
 		var start_change = $('.navbar');
 			start_change.toggleClass('is-fill', $(this).scrollTop() > start_change.height());
 	};
@@ -33,9 +45,20 @@
 
     var inicio = function() {
         typed.start();
+        function resizes(){
+            $(window).on('resize', function () {
+                if ($(window).width() <= 920) {
+                    $('.demo_bg:nth-child(2)').css('display', 'none');
+                } else if ($(window).width() > 920) {
+                    $('.demo_bg:nth-child(2)').css('display', 'flex');
+                }
+            });
+        };
 
-        $('.demo_more a').text('Ver más').on('click', showDemos);
-        $(document).scroll(scrollMenu);
+        $('.demo_more a').text('+').on('click', showDemos);
+        $('.demo_more').fixTo('#demo', {top: 0, useNativeSticky:true});
+
+        $(document).scroll(scrollNav);
 	};
 
 	$(document).ready(inicio); 
