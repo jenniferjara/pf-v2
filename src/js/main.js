17228836513f8f20756f536d794a4894fbf57bb8
Jennifer Jara <Jennifer Jara>
2018-02-05 23:29:30 -0500

    add responsive view section demos

--- src/js/main.js
+++ src/js/main.js
@@ -11,25 +11,7 @@
         });
     };*/
 
-    var showDemos = function (e) {
-        e.preventDefault();
-        //$('.demo_bg:nth-child(2)').toggle('slow');
-        if ($(this).text() == "+") {
-            $(this).text("-");
-            $(this).parent().addClass('demo_more_tp');
-            $('.demo_bg:nth-child(2)').css('display', 'block');
-            $("html, body").animate({ scrollTop: ($('.demo_bg:nth-child(1)').offset().top + $('.demo_bg:nth-child(1)').height()) - 70 }, 1000);
-            resizes();
-        } else {
-            $(this).text('+');
-            $(this).parent().removeClass('demo_more_tp');
-            $("html, body").animate({ scrollTop: ($('.demo_bg:nth-child(1)').offset().top) - 70}, 1400);
-            $('.demo_bg:nth-child(2)').css('display', 'none');
-        }
-
-    };
-
-	var scrollNav = function() {
+   	var scrollNav = function() {
 		var start_change = $('.navbar');
 			start_change.toggleClass('is-fill', $(this).scrollTop() > start_change.height());
 	};
@@ -43,24 +25,20 @@
         loop: true
     });
 
-    var inicio = function() {
+    var startPage = function() {
         typed.start();
-        function resizes(){
-            $(window).on('resize', function () {
-                if ($(window).width() <= 920) {
-                    $('.demo_bg:nth-child(2)').css('display', 'none');
-                } else if ($(window).width() > 920) {
-                    $('.demo_bg:nth-child(2)').css('display', 'flex');
-                }
-            });
-        };
-
-        $('.demo_more a').text('+').on('click', showDemos);
-        $('.demo_more').fixTo('#demo', {top: 0, useNativeSticky:true});
+        $('.bxslider').bxSlider({
+            controls: false,
+            mode: 'vertical',
+            slideMargin: 30
+        });
 
         $(document).scroll(scrollNav);
+
+
 	};
 
-	$(document).ready(inicio); 
+	$(document).ready(startPage);
+
 
 })(window.jQuery);
