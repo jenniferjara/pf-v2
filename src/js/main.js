ad6cd9866d735425b17896d5f829680e37d01321
Jennifer Jara <lisbet.jara@gmail.com>
2018-04-11 15:52:34 -0500

    update descriptions

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,11 @@
 
     'use strict';
 
-
-
     var scrollMenus = function () {
-        var id = $(this).find('a').attr("href"), posi, ele, padding = 65;
+        var id = $(this).find('a').attr("href"), posi, ele, padding = 61;
         ele = $(id); posi = ($(ele).offset()||0).top - padding;
 
-        $('html, body').animate({scrollTop:posi}, 500);
+        $('html, body').animate({scrollTop:posi}, 1200);
     };
 
    	var scrollNav = function() {
@@ -32,7 +30,7 @@
         loop: true
     });
 
-    var startPage = function() {
+    var startPage = function () {
         typed.start();
         $('.bxslider').bxSlider({
             controls: false,
@@ -40,17 +38,21 @@
             slideMargin: 30
         });
 
-        $('li[class^="option-"]').mouseenter(move);
-        $('li[class^="option-"]').mouseleave(quiet);
+        $("li[class^='option-']").mouseenter(move);
+        $("li[class^='option-']").mouseleave(quiet);
 
         $(document).scroll(scrollNav);
 
         //language=JQuery-CSS
-        $('ul.nav.navbar-nav > li').on('click', scrollMenus);
-        $('ul.hero_links_main:nth-child(1) > li').on('click', scrollMenus);
+        $('ul.nav.navbar-nav > li').on("click", scrollMenus);
+        $('ul.hero_links_main:nth-child(1) > li').on("click", scrollMenus);
+        $('a.nav_top').on("click", function (e) {
+            e.preventDefault();
+            $('html, body').animate({scrollTop: 0}, 1200);
+        });
 
 
-	};
+    };
 
 	$(document).ready(startPage);
 
